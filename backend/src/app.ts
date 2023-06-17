6e1d370285ca4306230712c3b4adc83618d4f42a
Guillaume <boyser2015@gmail.com>
2023-06-17 21:13:40 +0200

    chore(backend): clean code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,8 +11,6 @@ connectdb()
 
 const port: number = parseInt(process.env.API_PORT || '3000', 10)
 
-console.log(process.env.TOKEN_SECRET)
-
 const app: Express = express()
 
 app.use(logger('dev'))
